72b685b1effc0d298c84abfb2197b69af3dc4954
Thejaswi <thejaswirl187@gmail.com>
2022-05-02 22:26:38 +0530

    Added: Employee Wage Problems

---
+++ src/com/bridgelabz/day_5problems/Employee_Wage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day_5problems;
+
+public class Employee_Wage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage problems");
+    }
+}
